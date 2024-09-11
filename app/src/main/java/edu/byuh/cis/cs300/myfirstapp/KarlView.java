6970588e7df29dd9bca554b125308f4988aafde9
thirstynellan <gmd2@byuh.edu>
2024-09-11 10:50:59 -1000

    added rectangles and lines and paint objects

--- app/src/main/java/edu/byuh/cis/cs300/myfirstapp/KarlView.java
+++ app/src/main/java/edu/byuh/cis/cs300/myfirstapp/KarlView.java
@@ -3,17 +3,36 @@ package edu.byuh.cis.cs300.myfirstapp;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class KarlView extends View {
 
+    private Paint momo;
+    private Paint alema;
+
     public KarlView(Context c) {
         super(c);
+        momo = new Paint();
+        momo.setColor(Color.BLUE);
+        momo.setStyle(Paint.Style.STROKE);
+        alema = new Paint();
+        alema.setColor(Color.RED);
     }
 
     @Override
     public void onDraw(Canvas c) {
         super.onDraw(c);
+        float w = getWidth();
+        float h = getHeight();
+        float rectLeft = w * 0.25f;
+        float rectRight = w * 0.5f;
+        float rectTop = h * 0.2f;
+        float rectBottom = h * 0.6f;
+        momo.setStrokeWidth(w * 0.01f);
+        alema.setStrokeWidth(w * 0.02f);
         c.drawColor(Color.GREEN);
+        c.drawRect(rectLeft, rectTop, rectRight, rectBottom, momo);
+        c.drawLine(w*0.4f, h*0.3f, w*0.8f, h*0.8f, alema);
     }
 }
